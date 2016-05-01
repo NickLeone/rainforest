c8b3d2d2b190a2676ed3ca25fb3a3d6b0410b4aa
Nick Leone <nleone3@me.com>
2016-05-01 18:00:10 -0400

    added devise views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ActiveRecord::Base
+  User.connection
+  before_validation :authenticate_user!
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
